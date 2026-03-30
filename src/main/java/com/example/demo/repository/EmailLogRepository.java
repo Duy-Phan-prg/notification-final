f3fcd1591a40fda033ddfd4b3571f4c94b25be74
Duy-Phan-prg <phanduy.prg@gmail.com>
2026-03-30 16:34:57 +0700

    Update EmailLogRepository.java

--- src/main/java/com/example/demo/repository/EmailLogRepository.java
+++ src/main/java/com/example/demo/repository/EmailLogRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.EmailLog;
+import com.example.demo.enums.EmailStatus;
+import com.example.demo.enums.EmailType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,9 +13,9 @@ public interface EmailLogRepository extends JpaRepository<EmailLog, Long> {
     
     Page<EmailLog> findByEmail(String email, Pageable pageable);
     
-    Page<EmailLog> findByType(String type, Pageable pageable);
+    Page<EmailLog> findByType(EmailType type, Pageable pageable);
     
-    Page<EmailLog> findByStatus(String status, Pageable pageable);
+    Page<EmailLog> findByStatus(EmailStatus status, Pageable pageable);
     
     @Query("SELECT COUNT(e) FROM EmailLog e WHERE e.status = 'SUCCESS'")
     Long countSuccessEmails();
